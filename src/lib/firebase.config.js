81a38013b89a3e28c3f713fa63c200802105990f
Oleksii Ishchenko <oleksii.ishchenko@ekreative.com>
2025-02-07 16:42:58 +0200

    init proj

--- src/lib/firebase.config.js
+++ src/lib/firebase.config.js
@@ -1,5 +1,3 @@
-throw new Error("Replace src/app/lib/firebase.config.js with your own config");
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -7,8 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // ...
+    apiKey: "AIzaSyBxuryueKkZIO25PW1-2p3hH7w81dauf70",
+    authDomain: "gemini-extension-3ccaa.firebaseapp.com",
+    projectId: "gemini-extension-3ccaa",
+    storageBucket: "gemini-extension-3ccaa.firebasestorage.app",
+    messagingSenderId: "907966084618",
+    appId: "1:907966084618:web:c078fd63a5859fc71fd1f3"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
